1fa8fe1d19b23e295b496ea987a6ed2f1e307cb9
Jose Martin Rios Payares <jmriosp17@gmail.com>
2021-10-07 22:38:07 -0500

    Se aelimina logica no requerida para la tarea en el controller venta

--- src/main/java/jmriosp/com/tarea01/controller/VentaController.java
+++ src/main/java/jmriosp/com/tarea01/controller/VentaController.java
@@ -54,35 +54,4 @@ public class VentaController {
         return ResponseEntity.created(location).build();
     }
 
-    @PutMapping
-    public ResponseEntity<VentaDTO> modificar(@RequestBody VentaDTO ventaDTORequest) throws Exception {
-        if (!service.existePorId(ventaDTORequest.getIdVenta()))
-            throw new ModeloNotFoundException("ID NO ENCONTRADO " + ventaDTORequest.getIdVenta());
-        return new ResponseEntity<>(mapper.map(
-                service.modificar(mapper.map(ventaDTORequest, Venta.class)), VentaDTO.class), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminar(@PathVariable("id") Integer id) throws Exception {
-        if (!service.existePorId(id))
-            throw new ModeloNotFoundException("ID NO ENCONTRADO " + id);
-        service.eliminar(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @GetMapping("/hateoas/{id}")
-    public EntityModel<VentaDTO> listarHateoasPorId(@PathVariable("id") Integer id) throws Exception {
-        if (!service.existePorId(id))
-            throw new ModeloNotFoundException("ID NO ENCONTRADO " + id);
-
-        EntityModel<VentaDTO> recurso = EntityModel.of(mapper.map(service.listarPorId(id), VentaDTO.class));
-        //localhost:8080/Venta/id
-        WebMvcLinkBuilder link1 = linkTo(methodOn(this.getClass()).listarPorId(id));
-        WebMvcLinkBuilder link2 = linkTo(methodOn(this.getClass()).listarHateoasPorId(id));
-        recurso.add(link1.withRel("venta-recurso1"));
-        recurso.add(link2.withRel("venta-recurso2"));
-
-        return recurso;
-    }
-
 }
